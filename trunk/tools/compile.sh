cd3aa3bb917f29464dbdfa5a83fcca98d4af4909
lordrex34 <lordrex34@7ebfc7ef-c1e9-1aab-f24e-c472da2fdc07>
2010-08-17 19:03:07 +0000

    Linux scripts update, thanks Mystick

--- trunk/tools/compile.sh
+++ trunk/tools/compile.sh
@@ -7,21 +7,23 @@
 trap finish 2
 pomxmlLSPath="../AL-Login"
 pomxmlGSPath="../AL-Game"
-buildxmlCMPath="../AL-Commons"
+pomxmlCMPath="../AL-Commons"
+pomxmlCSPath="../AL-CServer"
 
 if [ "$pomxmlLSPath" = "$pomxmlGSPath" ]; then
    echo "Problem with Path Server"
 else
    echo "The path of the LoginServer project is $pomxmlLSPath"
    echo "The path of the GameServer project is $pomxmlGSPath"
-   echo "The path of the Commons project is $buildxmlCMPath"
+   echo "The path of the Commons project is $pomxmlCMPath"
+   echo "The path of the ChatServer project is $pomxmlCSPath"
 fi
 
 # Choice between build and clean
 asktype()
 {
-   echo "Choose build (b) for building GameServer, LoginServer or Commons project"
-   echo "Choose clean (c) for cleaning GameServer, LoginServer or Commons project"
+   echo "Choose build (b) for building GameServer, LoginServer, ChatServer or Commons project"
+   echo "Choose clean (c) for cleaning GameServer, LoginServer, ChatServer or Commons project"
    echo "Choose quit (q) to quit this script"
    echo "Your choice, type: (b) build, (c) clean or (q) quit?"
    read TYPEPROMPT
@@ -32,37 +34,41 @@ asktype()
       *) asktype;;
    esac
 }
-# Choice between GS, LS and CM build
+# Choice between GS, LS, CS and CM build
 askbuild()
 {
    echo "Choose Commons (c) for install Commons project (first Step)"
    echo "Choose GameServer (g) for building GameServer project"
    echo "Choose LoginServer (l) for building LoginServer project"
+   echo "Choose ChatServer (s) for building ChatServer project"
    echo "Choose quit (q) to quit this script"
-   echo "Your choice, type: (g) GameServer, (l) LoginServer, (c) Commons or (q) quit?"
+   echo "Your choice, type: (g) GameServer, (l) LoginServer, (c) Commons, (s) ChatServer or (q) quit?"
    read BUILDPROMPT
    case "$BUILDPROMPT" in
       "g"|"G") gsbuild;;
       "l"|"L") lsbuild;;
       "c"|"C") cmbuild;;
+      "s"|"S") csbuild;;
       "q"|"Q") finish;;
       *) askbuild;;
    esac
 }
 
-# Choice between GS, LS and cm clean
+# Choice between GS, LS, CS and CM clean
 askclean()
 {
    echo "Choose GameServer (g) for cleaning GameServer project"
    echo "Choose LoginServer (l) for cleaning LoginServer project"
    echo "Choose Commons (c) for cleaning Commons project"
+   echo "Choose ChatServer (s) for cleaning ChatServer project"
    echo "Choose quit (q) to quit this script"
-   echo "Your choice, type: (g) GameServer, (l) LoginServer, (c) Commons or (q) quit?"
+   echo "Your choice, type: (g) GameServer, (l) LoginServer, (c) Commons, (s) ChatServer or (q) quit?"
    read CLEANPROMPT
    case "$CLEANPROMPT" in
       "g"|"G") gsclean;;
       "l"|"L") lsclean;;
       "c"|"C") cmclean;;
+      "s"|"S") csclean;;
       "q"|"Q") finish;;
       *) askclean;;
    esac
@@ -96,9 +102,16 @@ gsbuild()
 # Commons Build
 cmbuild()
 {
-   echo "Moving to folder $buildxmlCMPath"
-   cd $buildxmlCMPath
+   echo "Moving to folder $pomxmlCMPath"
+   cd $pomxmlCMPath
    mvn install
+}
+# Commons Build
+csbuild()
+{
+   echo "Moving to folder $pomxmlCSPath"
+   cd $pomxmlCSPath
+   mvn assembly:assembly
 }
 
 # LoginServer Clean
@@ -120,9 +133,15 @@ gsclean()
 # Commons Clean
 cmclean()
 {
-   echo "Moving to folder $buildxmlCMPath"
-   cd $buildxmlCMPath
+   echo "Moving to folder $pomxmlCMPath"
+   cd $pomxmlCMPath
    mvn clean
 }
-
+# Chat Server Clean
+csclean()
+{
+   echo "Moving to folder $pomxmlCSPath"
+   cd $pomxmlCSPath
+   mvn clean
+}
 asktype
