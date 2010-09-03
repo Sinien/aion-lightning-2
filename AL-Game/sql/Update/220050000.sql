e97f02833c3b38c95afb7a333cc995e3e0e70cc8
Blu3crim3s666 <Blu3crim3s666@gmail.com>
2010-09-03 01:57:55 +0000

    Some drops from 2.0 added from my SVN

---
+++ AL-Game/sql/Update/220050000.sql
@@ -0,0 +1,3 @@
+INSERT INTO droplist (mobId, itemId, min, max, chance) VALUES
+(214501, 168000035, 1, 1, 0.05);
+
---
+++ AL-Game/sql/Update/400010000.sql
@@ -0,0 +1,29 @@
+INSERT INTO droplist (mobId, itemId, min, max, chance) VALUES
+(251002, 186000030, 1, 1, 0.9),
+(251002, 186000096, 1, 1, 0.05),
+(251002, 186000031, 1, 2, 1);
+
+INSERT INTO droplist (mobId, itemId, min, max, chance) VALUES
+(251018, 186000030, 1, 1, 0.9),
+(251018, 186000096, 1, 1, 0.05),
+(251018, 186000031, 1, 2, 1);
+
+INSERT INTO droplist (mobId, itemId, min, max, chance) VALUES
+(251021, 186000030, 1, 1, 0.9),
+(251021, 186000096, 1, 1, 0.05),
+(251021, 186000031, 1, 2, 1);
+
+INSERT INTO droplist (mobId, itemId, min, max, chance) VALUES
+(251033, 186000030, 1, 1, 0.9),
+(251033, 186000096, 1, 1, 0.05),
+(251033, 186000031, 1, 2, 1);
+
+INSERT INTO droplist (mobId, itemId, min, max, chance) VALUES
+(251036, 186000030, 1, 1, 0.9),
+(251036, 186000096, 1, 1, 0.05),
+(251036, 186000031, 1, 2, 1);
+
+INSERT INTO droplist (mobId, itemId, min, max, chance) VALUES
+(251039, 186000030, 1, 1, 0.9),
+(251039, 186000096, 1, 1, 0.05),
+(251039, 186000031, 1, 2, 1);
